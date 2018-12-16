9aaf35bc51e8ee558228dbf436340fbec9f320e0
brieuc l'hélias <brieuc.lhelias@gmail.com>
2018-12-16 20:43:19 +0100

    DEV - dodecagone challenge

--- Meilleur Dev de France Septembre 2017 (Session 2)/dodecagone.py
+++ Meilleur Dev de France Septembre 2017 (Session 2)/dodecagone.py
@@ -1,3 +1,18 @@
+"""
+Let's say we have an input of N=9
+
+the output should look as follow;
+
+    .#######.
+    ##*****##
+    #**###**#
+    #*##*##*#
+    #*#***#*#
+    #*##*##*#
+    #**###**#
+    ##*****##
+    .#######.
+"""
 N = 9
 grid = [["." for i in range(N)] for j in range(N)]
 
@@ -15,13 +30,18 @@ def draw(grid):
     col_len = N
     row_len = N
     tour = 1
-    for i in range(N):
+
+    for i in range(N-1):
+
         for droite in range(row_start, col_len):
             grid[row_start][droite] = shapes[tour]
+
         for gauche in range(col_len-1, col_start, -1):
             grid[row_len-1][gauche] = shapes[tour]
+
         for bas in range(row_start+1, row_len):
             grid[bas][col_len-1] = shapes[tour]
+
         for haut in range(row_len-1, row_start, -1):
             grid[haut][col_start] = shapes[tour]
 
@@ -35,11 +55,13 @@ def draw(grid):
             grid[row_start][col_len-1] = shapes[3-tour]
             grid[row_len-1][col_start] = shapes[3-tour]
             grid[row_len-1][col_len-1] = shapes[3-tour]
+
         row_start += 1
         col_start += 1
         row_len -= 1
         col_len -= 1
         tour = 3 - tour
+
     return grid
 
 print_grille(draw(grid))
